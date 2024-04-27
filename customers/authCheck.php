6a87ce0dbaf6df4e2b526ae77de2d99b1a35e160
KaungZayY <kaungzayyan19@gmail.com>
2024-04-27 06:24:45 -0700

    bug fixes

--- customers/authCheck.php
+++ customers/authCheck.php
@@ -1,5 +1,5 @@
 <?php 
     if (!isset($_SESSION['customer_id'])) {
         echo"<script>window.alert('Unauthorized Access')</script>";
-        echo"<script>window.location='login.php'</script>";
+        echo"<script>window.location='/mobile_banking/customers/login.php'</script>";
     }
