e30a407f8af8414c9f92a5b992f1a0cc11d2e7b6
KaungZayY <kaungzayyan19@gmail.com>
2024-07-17 22:29:36 +0700

    dockerization

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "db";
+$username = "docker";
+$password = "123";
 $dbname = "mobile_banking";
 
 // Create connection
