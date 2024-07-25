ac0a06b7b67209d26d1af823efbcffbc01fe7eb0
KaungZayY <kaungzayyan19@gmail.com>
2024-07-25 10:15:41 +0700

    customer path fix

--- customers/home.php
+++ customers/home.php
@@ -1,7 +1,7 @@
 <?php
+    session_start(); 
     include("header.php");
     include("../connection.php");
-    session_start(); 
     include("authCheck.php");
 
     $customer_id = $_SESSION['customer_id'];
